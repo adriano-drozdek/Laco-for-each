e01e36abcc00ac04c9c23828479c52927a6e52d8
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2021-12-14 15:14:26 -0300

    Adicionando LISTA com for each

---
+++ src/aplicação/Listas.java
@@ -0,0 +1,23 @@
+package aplicação;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listas {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>();
+		
+		list.add("Maria");
+		list.add("Ana");
+		list.add("Pedro");
+		list.add("João");
+		
+		for (String x : list) {
+			System.out.println(x);
+		};
+
+	}
+
+}
